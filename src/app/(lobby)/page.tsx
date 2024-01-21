c1fe690995bcccbae764e65db6ecd2e9378e4463
Hushibul <nayeemninz786@gmail.com>
2024-01-21 14:12:21 +0600

    Next Starter Template

---
+++ src/app/(lobby)/page.tsx
@@ -0,0 +1,60 @@
+export default function Home() {
+  return (
+    <main className="container">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus vel
+      dolore architecto iusto beatae, nobis ullam fugiat maxime laborum
+      voluptatum quae temporibus sequi minima officia ratione, excepturi
+      reprehenderit sapiente in. Dolore eveniet, reiciendis vel tempore numquam
+      molestiae rerum obcaecati omnis saepe voluptatum nam cum, recusandae at
+      quod. Officia consequatur sequi repudiandae aliquam aut illum voluptates
+      magni, omnis eligendi obcaecati molestiae nesciunt saepe nostrum tenetur
+      quisquam eveniet consectetur architecto vel deserunt voluptatibus
+      distinctio harum voluptatem. Corrupti, dicta! Expedita inventore
+      voluptatibus rerum, perferendis sed nisi dolores maxime, laborum facilis
+      eaque maiores. Veniam, rerum vero? Placeat, aliquam eum optio laudantium
+      blanditiis, ex excepturi velit adipisci esse autem, fuga porro deserunt?
+      Eum, ipsa odio. Quisquam illum ipsam aliquid repellat, labore delectus
+      rerum quia qui officia sed quibusdam veritatis ratione aperiam dignissimos
+      at, suscipit ea vel provident quis animi? Aperiam omnis ex rem, corrupti
+      voluptatibus optio, quidem, expedita tempora accusantium soluta amet
+      aspernatur! Facere, illum veritatis error possimus optio amet perferendis
+      nostrum deleniti? A blanditiis amet in libero nihil sit officia est
+      deserunt, rerum eligendi nostrum distinctio ipsum. Officia delectus a
+      laboriosam natus veniam veritatis iusto ratione ipsa quaerat, atque
+      doloribus voluptas quas, vitae quo inventore ex reiciendis et porro libero
+      nihil, commodi saepe explicabo eaque! Beatae reprehenderit voluptate sed
+      repellendus, velit commodi iusto mollitia, minima soluta similique magnam
+      alias aspernatur? Doloribus mollitia repellendus quidem, aut quibusdam quo
+      aliquid aliquam eos voluptate voluptas! Dolores beatae consequuntur
+      deserunt excepturi ipsum repellendus reprehenderit rerum blanditiis
+      aliquam totam saepe ipsa quis incidunt earum mollitia minima expedita
+      nesciunt neque, recusandae ex id sunt. Cupiditate expedita ea qui
+      asperiores. Temporibus alias blanditiis, similique neque cum ipsam, a
+      officia et tenetur assumenda incidunt placeat quae ipsa voluptatem nam ad
+      hic, veniam voluptates quas voluptas eveniet facere sed qui maxime. Nisi
+      molestiae porro consequatur reprehenderit excepturi eveniet laborum eius
+      repellendus animi delectus ratione recusandae magni dignissimos expedita
+      tenetur, libero aperiam minima quos. Laboriosam soluta eaque reiciendis
+      nam commodi! Optio sit aperiam enim unde tempore odio dolorum eius officia
+      iusto fuga hic ipsa voluptatum nobis repudiandae, autem nam, quibusdam
+      doloribus totam, accusamus harum quam neque soluta consequatur. Cum
+      tempora unde nesciunt eius possimus praesentium earum perferendis
+      dignissimos sunt explicabo quia, deserunt, similique consequatur minima
+      enim autem animi consequuntur delectus officiis necessitatibus minus?
+      Aliquid magni animi eaque ipsam quae vitae voluptate nesciunt magnam, hic
+      consequatur unde voluptates dolorum sed eum ducimus voluptatum earum
+      dolore quas nostrum eius vel eveniet sunt odit. Temporibus asperiores
+      dolores impedit provident delectus. Veniam nam inventore numquam
+      exercitationem voluptas ipsam explicabo non aperiam ullam unde dolore
+      asperiores, quibusdam alias ipsa libero blanditiis error delectus. Dolore
+      quaerat quae odit? Repellendus voluptatum optio beatae. Iste, est ab.
+      Beatae possimus aspernatur, distinctio at dolores aliquid quis officia
+      nobis? Numquam doloribus, eligendi, aut excepturi voluptas eaque aliquid
+      alias beatae in expedita, atque quae ipsum iusto ratione tempore rem
+      dolores. Sunt, eveniet ex cumque libero deleniti quam autem omnis
+      repudiandae impedit, nobis explicabo. Voluptatum ipsam accusantium
+      quaerat. Ipsam explicabo sint tempore? Corporis nihil sint modi est
+      officia, hic illum. Ab accusantium ex sit? Corrupti, magnam.
+    </main>
+  );
+}
